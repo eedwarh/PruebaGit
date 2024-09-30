a1799891422b666d1e927c6022e5817a5256deaf
garheredw <garheredw@PC20519.iespiringalla.ad>
2024-09-30 10:35:06 +0200

    1 Commit Rama 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Trabajando en la rama 1");
+		System.out.println("Trabajando en la rama 2");
 		
 	}
 }
